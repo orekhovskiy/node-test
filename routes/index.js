df3b596bdbc609992d14055450407be7dd52dfbc
Anton Orekhovskiy <anton.o.0619@gmail.com>
2022-08-04 22:59:22 +0300

    add indexview

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Save these values!',
+    inputTextValues: [
+      'first',
+      'second',
+      'third'
+    ],
+    saveUrl: '/save'
+  });
 });
 
 module.exports = router;
